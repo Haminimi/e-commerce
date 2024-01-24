6fb2532f5ec464417ba7f0da921cd4b0b844931d
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-25 00:58:13 +0100

    Update the Cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,20 +12,38 @@ const Cart = ({ cart }) => {
 		return (
 			<div className={styles.cartContainer}>
 				<h1>Your cart is empty. Add some items in the shop. 🛒</h1>
+
+				<a href="https://github.com/Haminimi" className={styles.link}>
+					<i
+						className={`devicon-github-original ${styles.gitHubIcon}`}
+					></i>
+				</a>
 			</div>
 		);
+	}
+
+	function openModal() {
+		const modal = document.querySelector('dialog');
+		modal.showModal();
+	}
+
+	function closeModal() {
+		const modal = document.querySelector('dialog');
+		modal.close();
 	}
 
 	return (
 		<div className={styles.cartContainer}>
 			<table>
-				{/* <caption>Cart</caption> */}
+				<caption className={styles.caption}>
+					<h2>Shopping Cart</h2>
+				</caption>
 				<thead>
 					<tr>
-						<th colSpan="1">Item</th>
-						<th colSpan="1">Price</th>
-						<th colSpan="1">Quantity</th>
-						<th colSpan="1">Total</th>
+						<th scope="col">Item</th>
+						<th scope="col">Price</th>
+						<th scope="col">Quantity</th>
+						<th scope="col">Total per item</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -44,13 +62,44 @@ const Cart = ({ cart }) => {
 						);
 					})}
 					<tr>
-						<th colSpan={3}>Total to pay</th>
+						<th colSpan={3} scope="row">
+							Total
+						</th>
 						<td className={styles.total}>
-							<b>{Math.floor(total)} $</b>
+							<b>{Number(total.toFixed(2))} $</b>
 						</td>
 					</tr>
 				</tbody>
 			</table>
+
+			<div className={styles.iconAndButton}>
+				<a href="https://github.com/Haminimi" className={styles.link}>
+					<i
+						className={`devicon-github-original ${styles.gitHubIcon}`}
+					></i>
+				</a>
+				<button onClick={openModal} className={styles.button}>
+					Buy
+				</button>
+			</div>
+
+			<dialog className={styles.modal}>
+				<div className={styles.modalContent}>
+					<h2>Thank You for Shopping with Us! ❤️</h2>
+					<p>
+						We express our sincere gratitude for choosing
+						AwesomeShop for your recent shopping spree! Your support
+						means the world to us, and we hope you found everything
+						you were looking for.
+					</p>
+					<span
+						onClick={closeModal}
+						className={`material-symbols-outlined ${styles.closeIcon}`}
+					>
+						close
+					</span>
+				</div>
+			</dialog>
 		</div>
 	);
 };
