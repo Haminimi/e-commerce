6a783a31e6a78787a5e02047c5e1ef47bad02070
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-25 19:00:27 +0100

    Update the ErrorPage component
    Change the error message.

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -4,9 +4,12 @@ import styles from './ErrorPage.module.css';
 const ErrorPage = () => {
 	return (
 		<div className={styles.errorPage}>
-			<h1>Oh no, this route doesn&apos;t exist!</h1>
-			<Link to="/">
-				You can go back to the home page by clicking here, though!
+			<h1>
+				Oh no, this route doesn&apos;t exist or there was a network
+				error encountered. Sorry. ❤️{' '}
+			</h1>
+			<Link className={styles.link} to="/">
+				You can go back to the home page by clicking here!
 			</Link>
 		</div>
 	);
