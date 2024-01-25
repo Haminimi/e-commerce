4ea8603474d6ea9fcd6ca1d023902098a6ab5914
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-25 18:46:28 +0100

    Add the Loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,15 @@
+import styles from './Loading.module.css';
+
+const Loading = () => {
+	return (
+		<div className={styles.loadingScreen}>
+			<div className={styles.spinner}>
+				<div className={styles.firstBall}></div>
+				<div className={styles.secondBall}></div>
+				<div className={styles.thirdBall}></div>
+			</div>
+		</div>
+	);
+};
+
+export default Loading;
