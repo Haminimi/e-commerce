7c831223bf0e52e129b6581d3764ed051596acf4
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-26 18:29:39 +0100

    Add a test for the Header component

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Cart from '../components/Cart/Cart';
 import Home from '../components/Home/Home';
+import Header from '../components/Header/Header';
 import { expect, vi } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
+import Router from '../Router';
+import { act } from 'react-dom/test-utils';
 
 describe('Cart component', () => {
 	it('should render a heading for the empty cart', () => {
@@ -91,7 +95,7 @@ describe('Cart component', () => {
 	});
 });
 
-describe.only('Home component', () => {
+describe('Home component', () => {
 	it('should render a welcome heading', () => {
 		render(<Home />);
 		const welcomeHeading = screen.getByRole('heading', { name: 'Welcome' });
@@ -110,5 +114,56 @@ describe.only('Home component', () => {
 		render(<Home />);
 		const link = screen.getByRole('link', { name: 'Artem Gavrysh' });
 		expect(link).toBeInTheDocument();
+	});
+});
+
+describe.only('Header component', () => {
+	it('should render the store name', () => {
+		render(
+			<Router>
+				<Header path="/" numberOfItems={0} />
+			</Router>
+		);
+		const header = screen.getByRole('heading', { name: 'AwesomeShop' });
+		expect(header).toBeInTheDocument();
+	});
+
+	describe('Navbar', () => {
+		it('should render the navbar', () => {
+			render(
+				<Router>
+					<Header path="/" numberOfItems={0} />
+				</Router>
+			);
+			const navbar = screen.getByRole('navigation');
+			expect(navbar).toBeInTheDocument();
+		});
+
+		it('should open the shop page on a click', async () => {
+			const user = userEvent.setup();
+
+			render(
+				<Router>
+					<Header path="/" numberOfItems={0} />
+				</Router>
+			);
+
+			const shopLink = screen.getByTestId('shop-link');
+
+			await user.click(shopLink);
+
+			expect(shopLink).toHaveClass('_activeLink_63c2ea');
+		});
+
+		it('should render the right number of items', () => {
+			render(
+				<BrowserRouter>
+					<Header path="/" numberOfItems={1} />
+				</BrowserRouter>
+			);
+
+			const numberOfItems = screen.getByTestId('number-of-items');
+			expect(numberOfItems).toHaveTextContent('1');
+		});
 	});
 });
